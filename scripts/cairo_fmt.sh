5ab3376e9faaa828b372bf7a55f37f4e98f88732
Marek Kaput <marek.kaput@swmansion.com>
2024-11-28 11:21:33 +0100

    Delete `scripts/`

--- scripts/cairo_fmt.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cargo run --profile=ci --bin cairo-format -- --recursive "$@"
--- scripts/docs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export RUSTUP_TOOLCHAIN="${RUSTUP_TOOLCHAIN:-nightly-2024-11-23}"
-
-RUSTDOCFLAGS="-Dwarnings" cargo doc --document-private-items --no-deps --all-features
