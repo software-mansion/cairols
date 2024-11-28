ce94d07e0f4fb38d6bdbd36245202a9bac6f31b7
Marek Kaput <marek.kaput@swmansion.com>
2024-11-28 11:21:33 +0100

    Delete `scripts/`

--- scripts/cairo_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cargo run --profile=ci --bin cairo-test -- corelib/ &&
-    cargo run --profile=ci --bin cairo-test -- tests/bug_samples/ --starknet
--- scripts/rust_fmt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export RUSTUP_TOOLCHAIN="${RUSTUP_TOOLCHAIN:-nightly-2024-11-23}"
-
-cargo fmt --all -- "$@"
