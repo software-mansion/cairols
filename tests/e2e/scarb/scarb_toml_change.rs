ca16a3fff2c30c101a0f4db8f6e9b99ab71c145d
piotmag769 <piotrmagiera150@gmail.com>
2025-01-13 15:58:32 +0100

    Scarb test: removing dependency
    commit-id:4d859745

--- tests/e2e/scarb/scarb_toml_change.rs
+++ tests/e2e/scarb/scarb_toml_change.rs
@@ -20,6 +20,15 @@ cairo_lang_test_utils::test_file_test!(
     test_removing_member
 );
 
+cairo_lang_test_utils::test_file_test!(
+    removing_dependency,
+    "tests/test_data/scarb",
+    {
+        removing_dependency: "removing_dependency.txt"
+    },
+    test_removing_dependency
+);
+
 fn caps(base: ClientCapabilities) -> ClientCapabilities {
     ClientCapabilities {
         text_document: base.text_document.or_else(Default::default),
@@ -74,5 +83,52 @@ fn test_removing_member(
     TestRunnerResult::success(OrderedHashMap::from([
         ("Analyzed crates".to_string(), analyzed_crates),
         ("Analyzed crates diff after member removal".to_string(), analyzed_crates_diff),
+    ]))
+}
+
+fn test_removing_dependency(
+    inputs: &OrderedHashMap<String, String>,
+    _args: &OrderedHashMap<String, String>,
+) -> TestRunnerResult {
+    let mut ls = sandbox! {
+        files {
+            "a/Scarb.toml" => &inputs["a/Scarb.toml"],
+            "a/src/lib.cairo" => "",
+            "b/Scarb.toml" => &inputs["b/Scarb.toml"],
+            "b/src/lib.cairo" => "",
+        }
+        client_capabilities = caps;
+    };
+
+    assert!(ls.open_and_wait_for_diagnostics("a/src/lib.cairo").diagnostics.is_empty());
+    // Check if opening `a` triggers calculating diagnostics for `b`.
+    assert!(ls.wait_for_diagnostics("b/src/lib.cairo").diagnostics.is_empty());
+
+    let analyzed_crates = ls.send_request::<lsp::ext::ViewAnalyzedCrates>(());
+    let analyzed_crates = normalize(&ls, analyzed_crates);
+
+    ls.edit_file("a/Scarb.toml", &inputs["a/Scarb.toml after dependency removal"]);
+    ls.send_notification::<DidChangeWatchedFiles>(DidChangeWatchedFilesParams {
+        changes: vec![FileEvent {
+            uri: ls.doc_id("a/Scarb.toml").uri,
+            typ: FileChangeType::CHANGED,
+        }],
+    });
+    ls.wait_for_project_update();
+
+    // FIXME(#90): `b` should disappear from the project model - `CrateId` representing `b`
+    //  should be removed from db.crate_configs().
+    let analyzed_crates_after_dep_removal = ls.send_request::<lsp::ext::ViewAnalyzedCrates>(());
+    let analyzed_crates_after_dep_removal = normalize(&ls, analyzed_crates_after_dep_removal);
+
+    let analyzed_crates_diff =
+        TextDiff::from_lines(&analyzed_crates, &analyzed_crates_after_dep_removal)
+            .unified_diff()
+            .context_radius(5)
+            .to_string();
+
+    TestRunnerResult::success(OrderedHashMap::from([
+        ("Analyzed crates".to_string(), analyzed_crates),
+        ("Analyzed crates diff after dependency removal".to_string(), analyzed_crates_diff),
     ]))
 }
