92ef9b0d752ede01d3299a103e39115346dd6e64
Piotr Magiera <56825108+piotmag769@users.noreply.github.com>
2025-01-15 16:12:45 +0100

    Scarb: test unmanaged core (#175)

--- tests/e2e/scarb/mod.rs
+++ tests/e2e/scarb/mod.rs
@@ -1,2 +1,3 @@
 mod scarb_toml_change;
 mod simple_deps;
+mod unmanaged_core;
---
+++ tests/e2e/scarb/unmanaged_core.rs
@@ -0,0 +1,48 @@
+use cairo_language_server::lsp;
+use indoc::indoc;
+
+use crate::support::normalize::normalize;
+use crate::support::sandbox;
+
+#[test]
+fn test_unmanaged_core_on_invalid_scarb_toml() {
+    let mut ls = sandbox! {
+        files {
+            "Scarb.toml" => indoc! { r#"
+                [package]
+                version = "0.1.0"
+            "#},
+            "src/lib.cairo" => "",
+        }
+    };
+
+    ls.open_and_wait_for_project_update("src/lib.cairo");
+
+    let analyzed_crates = ls.send_request::<lsp::ext::ViewAnalyzedCrates>(());
+
+    pretty_assertions::assert_eq!(normalize(&ls, analyzed_crates), indoc! {r#"
+            # Analyzed Crates
+
+            - `core`: `["[SCARB_REGISTRY_STD]/core/src/lib.cairo"]`
+                ```rust
+                CrateSettings {
+                    name: None,
+                    edition: V2024_07,
+                    version: Some(
+                        Version {
+                            major: 2,
+                            minor: 9,
+                            patch: 2,
+                        },
+                    ),
+                    cfg_set: None,
+                    dependencies: {},
+                    experimental_features: ExperimentalFeaturesConfig {
+                        negative_impls: true,
+                        associated_item_constraints: true,
+                        coupons: true,
+                    },
+                }
+                ```
+        "#});
+}
