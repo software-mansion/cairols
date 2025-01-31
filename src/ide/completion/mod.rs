40c1d3ba403d542be78ca8c9e37c2520ce452544
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2025-01-31 18:41:41 +0100

    Remove `CompletionKind`
    commit-id:19314e02

--- src/ide/completion/mod.rs
+++ src/ide/completion/mod.rs
@@ -1,21 +1,20 @@
-use cairo_lang_semantic::items::us::get_use_path_segments;
-use cairo_lang_semantic::resolve::AsSegments;
-use cairo_lang_syntax::node::ast::{BinaryOperator, ExprBinary, ExprStructCtorCall, PathSegment};
-use cairo_lang_syntax::node::db::SyntaxGroup;
+use cairo_lang_syntax::node::ast::{
+    BinaryOperator, ExprBinary, ExprPath, ExprStructCtorCall, UsePathLeaf, UsePathSingle,
+};
 use cairo_lang_syntax::node::kind::SyntaxKind;
-use cairo_lang_syntax::node::{SyntaxNode, TypedSyntaxNode, ast};
-use cairo_lang_utils::Upcast;
+use cairo_lang_syntax::node::{TypedSyntaxNode, ast};
+use colon_colon::{expr_path, use_statement};
 use completions::{attribute_completions, struct_constructor_completions};
 use if_chain::if_chain;
 use lsp_types::{CompletionParams, CompletionResponse, CompletionTriggerKind};
 use mod_item::mod_completions;
-use tracing::debug;
 
-use self::completions::{colon_colon_completions, dot_completions, generic_completions};
+use self::completions::{dot_completions, generic_completions};
 use crate::lang::db::{AnalysisDatabase, LsSemanticGroup, LsSyntaxGroup};
 use crate::lang::lsp::{LsProtoGroup, ToCairo};
 use crate::lang::syntax::SyntaxNodeExt;
 
+mod colon_colon;
 mod completions;
 mod mod_item;
 
@@ -63,105 +62,47 @@ pub fn complete(params: CompletionParams, db: &AnalysisDatabase) -> Option<Compl
         }
     );
 
-    let old_way_completions = match completion_kind(db, node.clone()) {
-        CompletionKind::ColonColon(segments) if !segments.is_empty() => {
-            colon_colon_completions(db, module_file_id, lookup_items, segments)
-        }
-        _ if trigger_kind == CompletionTriggerKind::INVOKED => {
-            let result = attribute_completions(db, node.clone())
-                .or_else(|| mod_completions(db, node, file_id))
-                .unwrap_or_else(|| generic_completions(db, module_file_id, lookup_items));
+    // Temp fix, this is due to fact that some completions dont have match yet.
+    let mut was_single = false;
 
-            Some(result)
-        }
-        _ => None,
-    }?;
+    if_chain!(
+        if let Some(leaf) = node.ancestor_of_type::<UsePathLeaf>(db);
+        if let Some(use_completions) = use_statement(db, ast::UsePath::Leaf(leaf), module_file_id, lookup_items.clone());
 
-    if completions.is_empty() {
-        completions.extend(old_way_completions);
+        then {
+            completions.extend(use_completions);
+        }
+    );
+
+    if_chain!(
+        if let Some(single) = node.ancestor_of_type::<UsePathSingle>(db);
+        if {
+            was_single = true;
+            true
+        };
+        if let Some(use_completions) = use_statement(db, ast::UsePath::Single(single), module_file_id, lookup_items.clone());
+
+        then {
+            completions.extend(use_completions);
+        }
+    );
+
+    if_chain!(
+        if let Some(expr) = node.ancestor_of_type::<ExprPath>(db);
+        if let Some(expr_path_completions) = expr_path(db, &node, expr, module_file_id, lookup_items.clone());
+
+        then {
+            completions.extend(expr_path_completions);
+        }
+    );
+
+    if completions.is_empty() && trigger_kind == CompletionTriggerKind::INVOKED && !was_single {
+        let result = attribute_completions(db, node.clone())
+            .or_else(|| mod_completions(db, node, file_id))
+            .unwrap_or_else(|| generic_completions(db, module_file_id, lookup_items));
+
+        completions.extend(result);
     }
 
     Some(CompletionResponse::Array(completions))
-}
-
-enum CompletionKind {
-    ColonColon(Vec<PathSegment>),
-}
-
-fn completion_kind(db: &AnalysisDatabase, node: SyntaxNode) -> CompletionKind {
-    debug!("node.kind: {:#?}", node.kind(db));
-    match node.kind(db) {
-        SyntaxKind::TerminalColonColon => {
-            let parent = node.parent().unwrap();
-            debug!("parent.kind: {:#?}", parent.kind(db));
-            if parent.kind(db) == SyntaxKind::ExprPath {
-                return completion_kind_from_path_node(db, parent);
-            }
-            let grandparent = parent.parent().unwrap();
-            debug!("grandparent.kind: {:#?}", grandparent.kind(db));
-            if grandparent.kind(db) == SyntaxKind::ExprPath {
-                return completion_kind_from_path_node(db, grandparent);
-            }
-            let (use_ast, should_pop) = if parent.kind(db) == SyntaxKind::UsePathLeaf {
-                (ast::UsePath::Leaf(ast::UsePathLeaf::from_syntax_node(db, parent)), true)
-            } else if grandparent.kind(db) == SyntaxKind::UsePathLeaf {
-                (ast::UsePath::Leaf(ast::UsePathLeaf::from_syntax_node(db, grandparent)), true)
-            } else if parent.kind(db) == SyntaxKind::UsePathSingle {
-                (ast::UsePath::Single(ast::UsePathSingle::from_syntax_node(db, parent)), false)
-            } else if grandparent.kind(db) == SyntaxKind::UsePathSingle {
-                (ast::UsePath::Single(ast::UsePathSingle::from_syntax_node(db, grandparent)), false)
-            } else {
-                debug!("Generic");
-                return CompletionKind::ColonColon(vec![]);
-            };
-            let Ok(mut segments) = get_use_path_segments(db.upcast(), use_ast) else {
-                debug!("Generic");
-                return CompletionKind::ColonColon(vec![]);
-            };
-            if should_pop {
-                segments.pop();
-            }
-            debug!("ColonColon");
-            return CompletionKind::ColonColon(segments);
-        }
-        SyntaxKind::TerminalIdentifier => {
-            let parent = node.parent().unwrap();
-            debug!("parent.kind: {:#?}", parent.kind(db));
-            let grandparent = parent.parent().unwrap();
-            debug!("grandparent.kind: {:#?}", grandparent.kind(db));
-            if grandparent.kind(db) == SyntaxKind::ExprPath
-                && db.get_children(grandparent.clone())[0].stable_ptr() != parent.stable_ptr()
-            {
-                // Not the first segment.
-                debug!("Not first segment");
-                return completion_kind_from_path_node(db, grandparent);
-            }
-            if grandparent.kind(db) == SyntaxKind::UsePathLeaf {
-                let use_ast = ast::UsePathLeaf::from_syntax_node(db, grandparent);
-                let Ok(mut segments) =
-                    get_use_path_segments(db.upcast(), ast::UsePath::Leaf(use_ast))
-                else {
-                    debug!("Generic");
-                    return CompletionKind::ColonColon(vec![]);
-                };
-                segments.pop();
-                debug!("ColonColon");
-                return CompletionKind::ColonColon(segments);
-            }
-        }
-        _ => (),
-    }
-    debug!("Generic");
-    CompletionKind::ColonColon(vec![])
-}
-
-fn completion_kind_from_path_node(db: &AnalysisDatabase, parent: SyntaxNode) -> CompletionKind {
-    debug!("completion_kind_from_path_node: {}", parent.clone().get_text_without_trivia(db));
-    let expr = ast::ExprPath::from_syntax_node(db, parent);
-    debug!("has_tail: {}", expr.has_tail(db));
-    let mut segments = expr.to_segments(db);
-    if expr.has_tail(db) {
-        segments.pop();
-    }
-    CompletionKind::ColonColon(segments)
 }
