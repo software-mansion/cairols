bbb0b00be9d5ee8c68b1d386e634c68c5ad575cd
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2025-02-14 14:02:11 +0100

    Extract finding available traits for methods
    commit-id:e541ea79

--- src/ide/code_actions/add_missing_trait.rs
+++ src/ide/code_actions/add_missing_trait.rs
@@ -1,21 +1,15 @@
 use std::collections::HashMap;
 
-use cairo_lang_defs::ids::{ModuleId, NamedLanguageElementId};
-use cairo_lang_filesystem::ids::FileId;
+use cairo_lang_defs::ids::ModuleId;
 use cairo_lang_filesystem::span::TextOffset;
-use cairo_lang_semantic::db::SemanticGroup;
-use cairo_lang_semantic::items::function_with_body::SemanticExprLookup;
-use cairo_lang_semantic::lookup_item::LookupItemEx;
 use cairo_lang_syntax::node::{TypedStablePtr, TypedSyntaxNode, ast};
-use cairo_lang_utils::Upcast;
+use if_chain::if_chain;
 use lsp_types::{CodeAction, CodeActionKind, Range, TextEdit, Url, WorkspaceEdit};
-use tracing::debug;
 
 use crate::lang::analysis_context::AnalysisContext;
-use crate::lang::db::{AnalysisDatabase, LsSemanticGroup};
+use crate::lang::db::AnalysisDatabase;
 use crate::lang::lsp::{LsProtoGroup, ToLsp};
-use crate::lang::methods::find_methods_for_type;
-use crate::lang::syntax::SyntaxNodeExt;
+use crate::lang::methods::available_traits_for_method;
 
 /// Create a Quick Fix code action to add a missing trait given a `CannotCallMethod` diagnostic.
 pub fn add_missing_trait(
@@ -23,78 +17,38 @@ pub fn add_missing_trait(
     ctx: &AnalysisContext<'_>,
     uri: Url,
 ) -> Option<Vec<CodeAction>> {
-    let file_id = db.file_for_url(&uri)?;
+    let trait_paths = available_traits_for_method(db, ctx)?;
 
-    missing_traits_actions(db, file_id, ctx, uri)
-}
+    let module_start_offset = if_chain! {
+        if let ModuleId::Submodule(submodule_id) = ctx.module_id;
+        if let ast::MaybeModuleBody::Some(body) = submodule_id.stable_ptr(db).lookup(db).body(db);
 
-/// Returns a list of code actions to add missing traits to the current module, or `None` if the
-/// type is missing.
-fn missing_traits_actions(
-    db: &AnalysisDatabase,
-    file_id: FileId,
-    ctx: &AnalysisContext<'_>,
-    uri: Url,
-) -> Option<Vec<CodeAction>> {
-    let syntax_db = db.upcast();
-    let function_with_body = ctx.lookup_item_id?.function_with_body()?;
-    let resolver = ctx.resolver(db);
-
-    let expr_node = ctx.node.ancestor_of_type::<ast::ExprBinary>(db)?.lhs(db);
-    let stable_ptr = expr_node.stable_ptr().untyped();
-    // Get its semantic model.
-    let expr_id = db.lookup_expr_by_ptr(function_with_body, expr_node.stable_ptr()).ok()?;
-    let semantic_expr = db.expr_semantic(function_with_body, expr_id);
-    // Get the type.
-    let ty = semantic_expr.ty();
-    if ty.is_missing(db) {
-        debug!("type is missing");
-        return None;
-    }
-
-    let module_start_offset = if let Some(ModuleId::Submodule(submodule_id)) =
-        db.find_module_containing_node(&ctx.node)
-    {
-        let module_def_ast = submodule_id.stable_ptr(db.upcast()).lookup(syntax_db);
-        if let ast::MaybeModuleBody::Some(body) = module_def_ast.body(syntax_db) {
-            body.items(syntax_db).as_syntax_node().span_start_without_trivia(syntax_db)
+        then {
+            body.items(db).as_syntax_node().span_start_without_trivia(db)
         } else {
             TextOffset::default()
         }
-    } else {
-        TextOffset::default()
     };
-    let module_start_position =
-        module_start_offset.position_in_file(db.upcast(), file_id).unwrap().to_lsp();
-    let relevant_methods = find_methods_for_type(db, resolver, ty, stable_ptr);
-    let current_module = db.find_module_file_containing_node(&ctx.node)?;
-    let module_visible_traits = db.visible_traits_from_module(current_module)?;
-    let unknown_method_name = ctx.node.get_text(db.upcast());
-    let mut code_actions = vec![];
 
-    for method in relevant_methods {
-        let method_name = method.name(db.upcast());
-        if method_name == unknown_method_name {
-            if let Some(trait_path) = module_visible_traits.get(&method.trait_id(db.upcast())) {
-                code_actions.push(CodeAction {
-                    title: format!("Import {}", trait_path),
-                    kind: Some(CodeActionKind::QUICKFIX),
-                    edit: Some(WorkspaceEdit {
-                        changes: Some(HashMap::from_iter([(
-                            uri.clone(),
-                            vec![TextEdit {
-                                range: Range::new(module_start_position, module_start_position),
-                                new_text: format!("use {};\n", trait_path),
-                            }],
-                        )])),
-                        document_changes: None,
-                        change_annotations: None,
-                    }),
-                    diagnostics: None,
-                    ..Default::default()
-                });
-            }
-        }
-    }
+    let file_id = db.file_for_url(&uri)?;
+    let module_start_position = module_start_offset.position_in_file(db, file_id)?.to_lsp();
+    let range = Range::new(module_start_position, module_start_position);
+
+    let code_actions = trait_paths
+        .into_iter()
+        .map(|trait_path| CodeAction {
+            title: format!("Import {}", trait_path),
+            kind: Some(CodeActionKind::QUICKFIX),
+            edit: Some(WorkspaceEdit {
+                changes: Some(HashMap::from_iter([(
+                    uri.clone(),
+                    vec![TextEdit { range, new_text: format!("use {};\n", trait_path) }],
+                )])),
+                ..Default::default()
+            }),
+            ..Default::default()
+        })
+        .collect();
+
     Some(code_actions)
 }
