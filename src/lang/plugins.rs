9505a62ebb905f76f2dd5b619fb9622739aa2319
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2025-02-24 13:22:02 +0100

    Fix dynamic disabling lint
    close #350
    
    commit-id:a4760447

--- src/lang/plugins.rs
+++ src/lang/plugins.rs
@@ -2,7 +2,11 @@ use cairo_lang_semantic::plugin::AnalyzerPlugin;
 
 pub trait AnalyzerPluginType: AnalyzerPlugin {
     fn is_cairo_lint_plugin(&self) -> bool {
-        format!("{self:?}").starts_with("CairoLint ")
+        let name = format!("{self:?}");
+        // Workaround for case when this was called on [`cairo_lang_semantic::ids::AnalyzerPluginLongId`]
+        let name = name.strip_prefix("AnalyzerPluginLongId(").unwrap_or(&name);
+
+        name.starts_with("CairoLint ")
     }
 }
 
