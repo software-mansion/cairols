e9991c5d85f66f430c1b3fcb9b853d37b50de89c
Jan Smółka <jp.smolka@gmail.com>
2025-02-25 20:21:53 +0100

    Add tests for `snforge` macros
    commit-id:b2a24b21

--- tests/e2e/macros/fixtures.rs
+++ tests/e2e/macros/fixtures.rs
@@ -1,10 +1,12 @@
-use indoc::formatdoc;
+use indoc::{formatdoc, indoc};
 
 use crate::support::fixture::{Fixture, fixture};
 
 use super::{MacroTest, SCARB_TEST_MACROS_PACKAGE};
 
 pub struct ProjectWithCustomMacros;
+pub struct ProjectWithSnforgeUnitTest;
+pub struct ProjectWithSnforgeIntegrationTest;
 
 impl MacroTest for ProjectWithCustomMacros {
     const CWD: &str = "test_package";
@@ -24,6 +26,60 @@ impl MacroTest for ProjectWithCustomMacros {
                 "#,
                 SCARB_TEST_MACROS_PACKAGE.display()
             )
+        }
+    }
+}
+
+impl MacroTest for ProjectWithSnforgeUnitTest {
+    const CWD: &str = "test_package";
+    const SNIPPET_LOCATION: &str = "test_package/src/lib.cairo";
+
+    fn fixture() -> Fixture {
+        fixture! {
+            "test_package/Scarb.toml" => indoc!(r#"
+                [package]
+                name = "test_package"
+                version = "0.1.0"
+                edition = "2024_07"
+
+                [tool.scarb]
+                allow-prebuilt-plugins = ["snforge_scarb_plugin"]
+
+                [dev-dependencies]
+                assert_macros = "2.10.0"
+                snforge_std = "0.37.0"
+                snforge_scarb_plugin = "0.37.0"
+            "#),
+        }
+    }
+}
+
+impl MacroTest for ProjectWithSnforgeIntegrationTest {
+    const CWD: &str = "test_package";
+    const SNIPPET_LOCATION: &str = "test_package/tests/test.cairo";
+
+    fn fixture() -> Fixture {
+        fixture! {
+            "test_package/lib.cairo" => "",
+
+            "test_package/Scarb.toml" => indoc!(r#"
+                [package]
+                name = "test_package"
+                version = "0.1.0"
+                edition = "2024_07"
+
+                [dev-dependencies]
+                snforge_std = "0.37.0"
+                snforge_scarb_plugin = "0.37.0"
+
+                [tool.scarb]
+                allow-prebuilt-plugins = ["snforge_scarb_plugin"]
+
+                [[tool.snforge.fork]]
+                name = "SEPOLIA_LATEST"
+                url = "https://starknet-sepolia.public.blastapi.io/rpc/v0_7"
+                block_id.tag = "latest"
+            "#),
         }
     }
 }
---
+++ tests/e2e/macros/procedural/snforge/mod.rs
@@ -0,0 +1,51 @@
+use crate::{
+    macros::fixtures::{ProjectWithSnforgeIntegrationTest, ProjectWithSnforgeUnitTest},
+    test_macro_expansion_and_diagnostics,
+};
+
+#[test]
+fn unit_test_simple() {
+    test_macro_expansion_and_diagnostics!(
+        ProjectWithSnforgeUnitTest,
+        r#"
+        #[cfg(test)]
+        mod tests {
+            #[test]<caret>
+            fn test_nothing() {
+                <caret>assert(1 == 1, 'Who knows');
+            }
+        }
+        "#
+    );
+}
+
+#[test]
+fn unit_test_with_assert_macro() {
+    test_macro_expansion_and_diagnostics!(
+        ProjectWithSnforgeUnitTest,
+        r#"
+        #[cfg(test)]
+        mod tests {
+            #[test]<caret>
+            fn test_nothing() {
+                <caret>assert_eq!(1, 1, "Who knows");
+            }
+        }
+        "#
+    );
+}
+
+#[test]
+fn integration_test_with_fork_and_fuzzer() {
+    test_macro_expansion_and_diagnostics!(
+        ProjectWithSnforgeIntegrationTest,
+        r#"
+        #[test]<caret>
+        #[fork(<caret>"SEPOLIA_LATEST")]
+        #[fuzzer(runs: 100,<caret> seed: 0x1234)]
+        fn test_nothing(x: felt252) {
+            <caret>assert(x == x, 'Who knows');
+        }
+        "#
+    );
+}
