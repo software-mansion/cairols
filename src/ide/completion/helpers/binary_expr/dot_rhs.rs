69eddac30e7890050807b1eac2924a25ff7f1a8d
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2025-03-06 16:48:29 +0100

    Filter `vars` and `params` in completions
    commit-id:d0a00b46
    
    WIP fix vars

---
+++ src/ide/completion/helpers/binary_expr/dot_rhs.rs
@@ -0,0 +1,20 @@
+use crate::lang::db::AnalysisDatabase;
+use cairo_lang_syntax::node::{
+    SyntaxNode, TypedSyntaxNode,
+    ast::{BinaryOperator, ExprBinary},
+};
+use if_chain::if_chain;
+
+pub fn dot_expr_rhs(db: &AnalysisDatabase, node: &SyntaxNode) -> Option<ExprBinary> {
+    if_chain!(
+        if let Some(binary_expression) = node.ancestor_of_type::<ExprBinary>(db);
+        if let BinaryOperator::Dot(_) = binary_expression.op(db);
+        if node.is_descendant(&binary_expression.rhs(db).as_syntax_node());
+
+        then {
+            Some(binary_expression)
+        } else {
+            None
+        }
+    )
+}
--- src/ide/completion/helpers/mod.rs
+++ src/ide/completion/helpers/mod.rs
@@ -1,1 +1,2 @@
+pub mod binary_expr;
 pub mod completion_kind;
