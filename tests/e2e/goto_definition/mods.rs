0b1dcc08d4126d666e40130c9be0782fcc188b46
piotmag769 <piotrmagiera150@gmail.com>
2025-03-07 17:16:51 +0100

    Improve UX of goto on non-inline modules
    Fixes #266
    
    commit-id:bb68535d

--- tests/e2e/goto_definition/mods.rs
+++ tests/e2e/goto_definition/mods.rs
@@ -1,7 +1,8 @@
 use indoc::indoc;
 
-use crate::goto_definition::GotoDefinitionTest;
+use crate::goto_definition::{GotoDefinitionTest, goto_definition};
 use crate::support::cairo_project_toml::CAIRO_PROJECT_TOML_2024_07;
+use crate::support::insta::test_transform;
 use crate::support::{cursors, fixture};
 
 #[test]
@@ -25,5 +26,126 @@ fn item_defined_in_another_file() {
     insta::assert_snapshot!(result, @r"
     // → src/something.cairo
     pub fn <sel>hello</sel>() {}
+    ")
+}
+
+#[test]
+fn non_inline_module_on_definition() {
+    let (lib_cairo, cursors) = cursors(indoc! {r#"
+        use crate::something::hello;
+
+        mod some<caret>thing;
+
+        fn main() {
+            hello();
+        }
+    "#});
+
+    let mut test = GotoDefinitionTest::begin(fixture! {
+        "cairo_project.toml" => CAIRO_PROJECT_TOML_2024_07,
+        "src/lib.cairo" => lib_cairo,
+        "src/something.cairo" => "pub fn hello() {\n}",
+    });
+
+    let result = test.request_snapshot("src/lib.cairo", cursors.caret(0));
+
+    insta::assert_snapshot!(result, @r"
+    // → src/something.cairo
+    <sel>pub fn hello() {
+    }</sel>
+    ")
+}
+
+#[test]
+fn non_inline_module_on_usage() {
+    let (lib_cairo, cursors) = cursors(indoc! {r#"
+        use crate::som<caret>ething::hello;
+
+        mod something;
+
+        fn main() {
+            hello();
+        }
+    "#});
+
+    let mut test = GotoDefinitionTest::begin(fixture! {
+        "cairo_project.toml" => CAIRO_PROJECT_TOML_2024_07,
+        "src/lib.cairo" => lib_cairo,
+        "src/something.cairo" => "pub fn hello() {\n}",
+    });
+
+    let result = test.request_snapshot("src/lib.cairo", cursors.caret(0));
+
+    insta::assert_snapshot!(result, @r"
+    // → src/something.cairo
+    <sel>pub fn hello() {
+    }</sel>
+    ")
+}
+
+#[test]
+fn inline_module_on_usage() {
+    test_transform!(goto_definition, r#"
+    use crate::some<caret>thing::hello;
+
+    mod something {
+      fn hello() {}
+    }
+
+    fn main() {
+        hello();
+    }
+    "#, @r"
+    use crate::something::hello;
+
+    mod <sel>something</sel> {
+      fn hello() {}
+    }
+
+    fn main() {
+        hello();
+    }
+    ")
+}
+
+#[test]
+fn inline_module_on_definition() {
+    test_transform!(goto_definition, r#"
+    use crate::something::hello;
+
+    mod some<caret>thing {
+      fn hello() {}
+    }
+
+    fn main() {
+        hello();
+    }
+    "#, @r"
+    use crate::something::hello;
+
+    mod <sel>something</sel> {
+      fn hello() {}
+    }
+
+    fn main() {
+        hello();
+    }
+    ")
+}
+
+#[test]
+fn crate_module() {
+    test_transform!(goto_definition, r#"
+    use cra<caret>te::main;
+
+    fn main() {
+        hello();
+    }
+    "#, @r"
+    <sel>use crate::main;
+
+    fn main() {
+        hello();
+    }</sel>
     ")
 }
