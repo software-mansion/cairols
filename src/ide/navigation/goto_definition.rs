804a480ce307301b12c332066997c1e60914f1ec
Piotr Magiera <56825108+piotmag769@users.noreply.github.com>
2025-03-10 14:22:41 +0100

    Improve UX of goto on non-inline modules (#444)

--- src/ide/navigation/goto_definition.rs
+++ src/ide/navigation/goto_definition.rs
@@ -1,9 +1,14 @@
-use cairo_lang_utils::Upcast;
-use lsp_types::{GotoDefinitionParams, GotoDefinitionResponse, Location};
-
-use crate::lang::db::{AnalysisDatabase, LsSyntaxGroup};
+use crate::lang::db::{AnalysisDatabase, LsSemanticGroup, LsSyntaxGroup};
 use crate::lang::defs::SymbolDef;
-use crate::lang::lsp::{LsProtoGroup, ToCairo, ToLsp};
+use crate::lang::lsp::{LsProtoGroup, ToCairo};
+use cairo_lang_defs::db::DefsGroup;
+use cairo_lang_defs::ids::ModuleId;
+use cairo_lang_filesystem::ids::{FileId, FileLongId};
+use cairo_lang_filesystem::span::TextSpan;
+use cairo_lang_parser::db::ParserGroup;
+use cairo_lang_utils::LookupIntern;
+use lsp_types::{GotoDefinitionParams, GotoDefinitionResponse};
+use std::ops::Not;
 
 /// Get the definition location of a symbol at a given text document position.
 pub fn goto_definition(
@@ -15,11 +20,38 @@ pub fn goto_definition(
 
     let identifier = db.find_identifier_at_position(file, position)?;
     let symbol = SymbolDef::find(db, &identifier)?;
-    let (found_file, span) = symbol.definition_location(db)?;
 
-    let found_uri = db.url_for_file(found_file)?;
-    let range = span.position_in_file(db.upcast(), found_file)?.to_lsp();
-    let location = Location { uri: found_uri, range };
+    let (found_file, span) = try_special_case_non_inline_module(db, &symbol)
+        .map_or_else(|| symbol.definition_location(db), Some)?;
+
+    let location = db.lsp_location((found_file, span))?;
 
     Some(GotoDefinitionResponse::Scalar(location))
+}
+
+// In the case of a non-inline module redirect to a module file instead of a definition node.
+fn try_special_case_non_inline_module(
+    db: &AnalysisDatabase,
+    symbol: &SymbolDef,
+) -> Option<(FileId, TextSpan)> {
+    if let SymbolDef::Module(module_def) = symbol {
+        let module_id = module_def.module_id();
+        match module_id {
+            ModuleId::CrateRoot(_) => None,
+            ModuleId::Submodule(submodule_id) => db
+                .infallible_is_submodule_inline(submodule_id)
+                .not()
+                .then(|| {
+                    let file = db.module_main_file(module_def.module_id()).ok()?;
+
+                    match file.lookup_intern(db) {
+                        FileLongId::OnDisk(_) => Some((file, db.file_syntax(file).ok()?.span(db))),
+                        FileLongId::Virtual(_) | FileLongId::External(_) => None,
+                    }
+                })
+                .flatten(),
+        }
+    } else {
+        None
+    }
 }
