88b21129833a639b83c45ae8d3b5b34979d818c7
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2025-03-13 13:32:49 +0100

    Add code lens tests
    commit-id:6f6fbe1c

---
+++ tests/e2e/code_lens/no_runners.rs
@@ -0,0 +1,65 @@
+use crate::{code_lens::test_code_lens_no_runner, support::insta::test_transform};
+
+#[test]
+fn only_functions() {
+    test_transform!(test_code_lens_no_runner, r#"
+    #[test]
+    fn a() {}
+    #[test]<caret>
+    fn b() {}
+
+
+    #[test]
+    fn c() {}
+    "#, @"lenses = []")
+}
+
+#[test]
+fn fn_in_mod() {
+    test_transform!(test_code_lens_no_runner, r#"
+    mod b {
+        #[test]<caret>
+        fn a() {}
+    }
+    "#, @"lenses = []")
+}
+
+#[test]
+fn run_for_mod() {
+    test_transform!(test_code_lens_no_runner, r#"
+    mod b {<caret>
+        #[test]
+        fn a() {}
+    }
+    "#, @"lenses = []")
+}
+
+#[test]
+fn mod_without_test() {
+    test_transform!(test_code_lens_no_runner, r#"
+    mod b {<caret>
+        fn a() {}
+    }
+    "#, @"lenses = []")
+}
+
+#[test]
+fn complex() {
+    test_transform!(test_code_lens_no_runner, r#"
+    mod b {
+        fn a() {}
+    }
+
+    mod c {
+        #[test]
+        fn d() {}
+
+        fn e() {}
+    }
+
+    #[test]
+    fn f() {}
+
+    fn f() {}<caret>
+    "#, @"lenses = []")
+}
