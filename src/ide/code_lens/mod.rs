031681f2420bbcb5f1ee296982571b6382bb197c
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2025-03-18 16:13:56 +0100

    Code lens refresh support (#476)

--- src/ide/code_lens/mod.rs
+++ src/ide/code_lens/mod.rs
@@ -1,10 +1,13 @@
 use crate::config::Config;
 use crate::lang::db::AnalysisDatabase;
-use crate::server::client::Notifier;
+use crate::server::client::{Notifier, Requester};
+use crate::server::schedule::Task;
 use crate::state::State;
+use lsp_types::request::CodeLensRefresh;
 use lsp_types::{CodeLens, Url};
 use serde_json::Value;
 use std::collections::HashMap;
+use std::collections::hash_map::Entry;
 use std::sync::{Arc, RwLock};
 use tests::TestCodeLensProvider;
 
@@ -21,25 +24,86 @@ pub struct CodeLensController {
 }
 
 impl CodeLensController {
+    pub fn on_did_change(
+        &self,
+        requester: &mut Requester<'_>,
+        db: &AnalysisDatabase,
+        config: &Config,
+        files: impl Iterator<Item = FileChange>,
+    ) {
+        let lens_guard = self.state.read().unwrap();
+
+        // If it was not requested before, there is nothing to invalidate.
+        let files: Vec<_> =
+            files.filter(|file_change| lens_guard.lens.contains_key(&file_change.url)).collect();
+
+        // Release so any panickable action is performed while not keeping state lock.
+        drop(lens_guard);
+
+        // Collect so any panickable action is performed while not keeping state lock.
+        let entries: Vec<_> = files
+            .into_iter()
+            .filter_map(|file_change| {
+                calculate_code_lens(file_change.url.clone(), db, config)
+                    .map(|code_lenses| (file_change, code_lenses))
+            })
+            .collect();
+
+        let mut lens_guard = self.state.write().unwrap();
+
+        let mut should_refresh = false;
+
+        for (file_change, code_lenses) in entries {
+            let entry = lens_guard.lens.entry(file_change.url.clone());
+
+            should_refresh = should_refresh
+                || match &entry {
+                    Entry::Occupied(occupied) if file_change.was_deleted => {
+                        !occupied.get().is_empty()
+                    }
+                    Entry::Occupied(occupied) => occupied.get() != &code_lenses,
+                    Entry::Vacant(_) => !code_lenses.is_empty(),
+                };
+
+            if file_change.was_deleted {
+                lens_guard.lens.remove(&file_change.url);
+            } else {
+                entry.insert_entry(code_lenses);
+            }
+        }
+
+        if should_refresh {
+            let _ = requester.request::<CodeLensRefresh>((), |_| Task::nothing());
+        }
+    }
+
     pub fn code_lens(
         &self,
         url: Url,
         db: &AnalysisDatabase,
         config: &Config,
     ) -> Option<Vec<CodeLens>> {
-        let result = calculate_code_lens(url.clone(), db, config)?;
+        let state = self.state.read().unwrap();
 
-        // Lock state only if calculating did *not* panicked, so the lock will not be poisoned.
-        let mut state = self.state.write().unwrap();
-        let entry = state.lens.entry(url);
+        let code_lens = if let Some(code_lens) = state.lens.get(&url) {
+            code_lens.iter().map(|(code_lens, _kind)| code_lens).cloned().collect()
+        } else {
+            drop(state);
 
-        let code_lens = entry
-            .insert_entry(result)
-            .get()
-            .iter()
-            .map(|(code_lens, _kind)| code_lens)
-            .cloned()
-            .collect();
+            let result = calculate_code_lens(url.clone(), db, config)?;
+
+            // Lock state only if calculating did *not* panic, so the lock will not be poisoned.
+            let mut state = self.state.write().unwrap();
+            let entry = state.lens.entry(url);
+
+            entry
+                .insert_entry(result)
+                .get()
+                .iter()
+                .map(|(code_lens, _kind)| code_lens)
+                .cloned()
+                .collect()
+        };
 
         Some(code_lens)
     }
@@ -59,6 +123,11 @@ impl CodeLensController {
     }
 }
 
+pub struct FileChange {
+    pub url: Url,
+    pub was_deleted: bool,
+}
+
 trait CodeLensProvider {
     fn calculate_code_lens(
         &self,
@@ -76,7 +145,7 @@ trait CodeLensProvider {
     ) -> Option<()>;
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq)]
 enum CodeLensKind {
     Test,
 }
