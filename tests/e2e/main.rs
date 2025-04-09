77221fb6fc2bc7d02fed5810f8210fe9fd0990f5
Jan Smółka <jp.smolka@gmail.com>
2025-04-09 15:17:32 +0200

    Unignore tests related to procedural macros

--- tests/e2e/main.rs
+++ tests/e2e/main.rs
@@ -1,19 +1,13 @@
-#![expect(dead_code)]
-
-// TODO(#539) Unignore tests.
-// They fail because PMS API is incompatible and requests get timeout.
-// Unignoring requires new Scarb nightly.
-
 mod analysis;
 mod code_actions;
-// mod code_lens;
+mod code_lens;
 mod completions;
 mod document_highlight;
 mod find_references;
 mod goto_definition;
 mod hover;
 mod linter;
-// mod macros;
+mod macros;
 mod no_config_reload;
 mod rename;
 mod scarb;
