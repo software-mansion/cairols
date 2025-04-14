6821f401c1305dfb0bfbf909019a2abc43ef615a
Arcticae <tomekgsd@gmail.com>
2025-04-14 10:18:57 +0200

    Change type of node to look for in trait impl functions. Skip if we're on function parameters

--- src/lang/defs/finder.rs
+++ src/lang/defs/finder.rs
@@ -66,8 +66,12 @@ fn try_impl_items(
         let trait_id = db.impl_def_concrete_trait(long_id).ok()?.trait_id(db);
 
         if let Some(function) =
-            identifier.as_syntax_node().ancestor_of_type::<ast::FunctionWithBody>(db)
+            identifier.as_syntax_node().ancestor_of_type::<ast::FunctionDeclaration>(db)
         {
+            // We don't want this to look for the trait definition on parameters themselves
+            if identifier.as_syntax_node().ancestor_of_type::<ast::ParamList>(db).is_some() {
+                return None;
+            }
             let impl_func_name = function.name(db).text(db);
             let trait_fn = db.trait_function_by_name(trait_id, impl_func_name).ok()??;
 
