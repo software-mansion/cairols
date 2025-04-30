39a2bb3a6095f87ac673011cb28f4c5cfc16bd46
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2025-04-30 16:57:16 +0200

    Use new type formatter in hover
    commit-id:f0e24160

--- src/ide/hover/mod.rs
+++ src/ide/hover/mod.rs
@@ -1,7 +1,8 @@
 use lsp_types::{Hover, HoverContents, HoverParams, MarkupContent, MarkupKind};
 
-use crate::lang::db::{AnalysisDatabase, LsSyntaxGroup};
+use crate::lang::db::{AnalysisDatabase, LsSemanticGroup, LsSyntaxGroup};
 use crate::lang::lsp::{LsProtoGroup, ToCairo};
+use cairo_lang_semantic::db::SemanticGroup;
 use cairo_lang_syntax::node::ast::TerminalUnderscore;
 
 mod render;
@@ -10,25 +11,25 @@ mod render;
 pub fn hover(params: HoverParams, db: &AnalysisDatabase) -> Option<Hover> {
     let file_id = db.file_for_url(&params.text_document_position_params.text_document.uri)?;
     let position = params.text_document_position_params.position.to_cairo();
+    let node = db.find_syntax_node_at_position(file_id, position)?;
 
-    if let Some(hover) = db
-        .find_syntax_node_at_position(file_id, position)
-        .and_then(|ref node| render::literal(db, node, file_id))
-    {
+    let importables =
+        db.visible_importables_from_module(db.find_module_file_containing_node(&node)?)?;
+
+    if let Some(hover) = render::literal(db, &node, file_id, &importables) {
         return Some(hover);
     }
 
-    if let Some(hover) = db
-        .find_syntax_node_at_position(file_id, position)
-        .and_then(|node| node.ancestor_of_type::<TerminalUnderscore>(db))
-        .and_then(|underscore| render::ty(db, underscore, file_id))
+    if let Some(hover) = node
+        .ancestor_of_type::<TerminalUnderscore>(db)
+        .and_then(|underscore| render::ty(db, underscore, file_id, &importables))
     {
         return Some(hover);
     }
 
     if let Some(hover) = db
         .find_identifier_at_position(file_id, position)
-        .and_then(|ref id| render::definition(db, id, file_id))
+        .and_then(|ref id| render::definition(db, id, file_id, &importables))
     {
         return Some(hover);
     }
