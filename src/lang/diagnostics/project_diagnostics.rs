6ad43fed4f488024ac8289eb2788aa8eeecd4941
Piotr Magiera <56825108+piotmag769@users.noreply.github.com>
2025-04-30 11:04:20 +0200

    Stop ignoring diagnostics from virtual files that are not mapped to on disk files (#608)
    Co-authored-by: Arcticae <tomekgsd@gmail.com>

--- src/lang/diagnostics/project_diagnostics.rs
+++ src/lang/diagnostics/project_diagnostics.rs
@@ -1,11 +1,8 @@
+use itertools::{Either, Itertools};
+use lsp_types::{Diagnostic, Url};
 use std::collections::{HashMap, HashSet};
 use std::mem;
 use std::sync::{Arc, RwLock};
-
-use itertools::{Either, Itertools};
-use lsp_types::Url;
-
-use crate::lang::diagnostics::file_diagnostics::FileDiagnostics;
 
 /// Global storage of diagnostics for the entire analysed codebase(s).
 ///
@@ -19,8 +16,23 @@ use crate::lang::diagnostics::file_diagnostics::FileDiagnostics;
 /// [`FileId`]: cairo_lang_filesystem::ids::FileId
 #[derive(Clone)]
 pub struct ProjectDiagnostics {
-    file_diagnostics: Arc<RwLock<HashMap<Url, FileDiagnostics>>>,
+    /// A map from an [`Url`] of an on disk file to diagnostics of the file and virtual files
+    /// originating from the file.
+    ///
+    /// ## Invariants
+    /// 1. Any [`Url`] key in the *outer* mapping **MUST** correspond to an on disk file.
+    /// 2. Any [`Url`] key in an *inner* mapping **MUST** be either:
+    ///    - equal to the [`Url`] key from the outer mapping
+    ///    - corresponding to a virtual file originating from the file with the [`Url`] key from the
+    ///      outer mapping
+    /// 3. Any [`Url`] key from any *inner* mapping **MUST** be unique amongst all the keys from all
+    ///    inner mappings. This invariant always holds if the previous one does.
+    file_diagnostics: Arc<RwLock<HashMap<Url, SelfAndOriginatingFilesDiagnostics>>>,
 }
+
+/// Diagnostics for a processed on disk file and virtual files originating from the processed file.
+/// Check [`crate::lang::diagnostics::file_diagnostics`] for more info.
+type SelfAndOriginatingFilesDiagnostics = HashMap<Url, Vec<Diagnostic>>;
 
 impl ProjectDiagnostics {
     /// Creates new project diagnostics instance.
@@ -28,42 +40,67 @@ impl ProjectDiagnostics {
         Self { file_diagnostics: Default::default() }
     }
 
-    /// Inserts new diagnostics for a file if they update the existing diagnostics.
+    /// Update existing diagnostics based on new diagnostics obtained by processing an on disk file.
     ///
-    /// Returns `true` if stored diagnostics were updated; otherwise, returns `false`.
-    pub fn insert(&self, diags: FileDiagnostics) -> bool {
-        if let Some(old_diags) = self
+    /// Returns mapping from file to its diagnostics for files which diagnostics changed
+    /// as a result of the update.
+    pub fn update(
+        &self,
+        processed_file_url: Url,
+        new_diags: SelfAndOriginatingFilesDiagnostics,
+    ) -> HashMap<Url, Vec<Diagnostic>> {
+        let old_diags = self
             .file_diagnostics
             .read()
             .expect("file diagnostics are poisoned, bailing out")
-            .get(&diags.url)
-        {
-            if *old_diags == diags {
-                return false;
-            }
-        };
+            .get(&processed_file_url)
+            .cloned()
+            .unwrap_or_default();
+
+        if new_diags == old_diags {
+            return HashMap::new();
+        }
 
         self.file_diagnostics
             .write()
             .expect("file diagnostics are poisoned, bailing out")
-            .insert(diags.url.clone(), diags);
-        true
+            .insert(processed_file_url.clone(), new_diags.clone());
+
+        let mut diags_to_send = HashMap::new();
+
+        for location_file_url in old_diags.keys() {
+            // If there are no diagnostics for a file that used to have diagnostics,
+            // we have to send empty diagnostics to the client.
+            if !new_diags.contains_key(location_file_url) {
+                diags_to_send.insert(location_file_url.clone(), Vec::new());
+            }
+        }
+
+        for (location_file_url, new_diags_for_url) in new_diags {
+            // If diagnostics have changed for a given file, we have to send an update.
+            if old_diags.get(&location_file_url) != Some(&new_diags_for_url) {
+                diags_to_send.insert(location_file_url, new_diags_for_url);
+            }
+        }
+
+        diags_to_send
     }
 
-    /// Removes diagnostics for files not present in the given set and returns a list of actually
-    /// removed entries.
-    pub fn clear_old(&self, files_to_retain: &HashSet<Url>) -> Vec<FileDiagnostics> {
+    /// Removes diagnostics for files not present in the given set and returns a list of files for
+    /// which diagnostics were actually cleared.
+    pub fn clear_old(&self, processed_files_to_retain: &HashSet<Url>) -> Vec<Url> {
         let mut file_diagnostics =
             self.file_diagnostics.write().expect("file diagnostics are poisoned, bailing out");
 
-        let (clean, removed) =
-            mem::take(&mut *file_diagnostics).into_iter().partition_map(|(file_url, diags)| {
-                if files_to_retain.contains(&file_url) {
-                    Either::Left((file_url, diags))
+        let (clean, removed) = mem::take(&mut *file_diagnostics).into_iter().partition_map(
+            |(processed_file_url, diags)| {
+                if processed_files_to_retain.contains(&processed_file_url) {
+                    Either::Left((processed_file_url, diags))
                 } else {
-                    Either::Right(diags)
+                    Either::Right(processed_file_url)
                 }
-            });
+            },
+        );
 
         *file_diagnostics = clean;
         removed
--- src/lang/diagnostics/refresh.rs
+++ src/lang/diagnostics/refresh.rs
@@ -1,16 +1,17 @@
-use std::collections::HashSet;
-
-use cairo_lang_defs::ids::ModuleId;
-use cairo_lang_filesystem::ids::FileId;
-use lsp_types::notification::PublishDiagnostics;
-use lsp_types::{DiagnosticSeverity, PublishDiagnosticsParams, Url};
-
 use crate::lang::db::AnalysisDatabase;
 use crate::lang::diagnostics::file_diagnostics::FileDiagnostics;
 use crate::lang::diagnostics::project_diagnostics::ProjectDiagnostics;
 use crate::lang::lsp::LsProtoGroup;
 use crate::server::client::Notifier;
 use crate::toolchain::scarb::ScarbToolchain;
+use cairo_lang_defs::ids::ModuleId;
+use cairo_lang_filesystem::db::FilesGroup;
+use cairo_lang_filesystem::ids::{FileId, FileLongId};
+use cairo_lang_utils::LookupIntern;
+use lsp_types::notification::PublishDiagnostics;
+use lsp_types::{DiagnosticSeverity, PublishDiagnosticsParams, Url};
+use std::collections::HashSet;
+use std::path::PathBuf;
 
 /// Refresh diagnostics and send diffs to the client.
 #[tracing::instrument(skip_all)]
@@ -57,21 +58,50 @@ fn refresh_file_diagnostics(
     };
 
     // IMPORTANT: DO NOT change the order of operations here. `to_lsp` may panic, so it has to come
-    // before `insert`. It is to make sure that if `insert` succeeds, `notify` executes as well.
-    let mut params = new_file_diagnostics.to_lsp(db, file, trace_macro_diagnostics);
+    // before `update`. It is to make sure that if `update` succeeds, `notify` executes as well.
+    let (processed_file_url, new_diags) = new_file_diagnostics.to_lsp(db, trace_macro_diagnostics);
 
-    // We want to ensure better UX by avoiding showing anything but errors from code that is not
-    // controlled by a user (dependencies from git/package register).
-    // Therefore, we filter non-error diagnostics for files residing in Scarb cache.
-    let is_dependency = scarb_toolchain.cache_path().is_some_and(|cache_path| {
-        params.uri.to_file_path().is_ok_and(|p| p.starts_with(cache_path))
-    });
-    if is_dependency {
-        params.diagnostics.retain(|diag| diag.severity == Some(DiagnosticSeverity::ERROR));
+    let new_diags = new_diags
+        .into_iter()
+        .filter_map(|((url, file_id), mut diagnostics)| {
+            // We want to ensure better UX by avoiding showing anything but errors from code that is
+            // not controlled by a user (dependencies from git/package register).
+            // Therefore, we filter non-error diagnostics for files residing in Scarb cache
+            // and virtual files originating from them.
+            let is_dependency = scarb_toolchain.cache_path().is_some_and(|cache_path| {
+                originating_file_path(db, file_id).is_some_and(|p| p.starts_with(cache_path))
+            });
+
+            if is_dependency {
+                diagnostics.retain(|diag| diag.severity == Some(DiagnosticSeverity::ERROR));
+                // Return early here to not filter out entry with empty diagnostics for
+                // the processed file if it happens to be here. It is necessary for our tests.
+                if diagnostics.is_empty() {
+                    return None;
+                }
+            }
+
+            Some((url, diagnostics))
+        })
+        .collect();
+
+    let diags_to_send = project_diagnostics.update(processed_file_url, new_diags);
+    for (url, diagnostics) in diags_to_send {
+        notifier.notify::<PublishDiagnostics>(PublishDiagnosticsParams {
+            uri: url,
+            diagnostics,
+            version: None,
+        });
     }
+}
 
-    if project_diagnostics.insert(new_file_diagnostics.clone()) {
-        notifier.notify::<PublishDiagnostics>(params);
+/// For an on disk file - returns a path to it.
+/// For a virtual file - returns a path to its first on disk ancestor.
+fn originating_file_path(db: &dyn FilesGroup, file_id: FileId) -> Option<PathBuf> {
+    match file_id.lookup_intern(db) {
+        FileLongId::OnDisk(path) => Some(path),
+        FileLongId::Virtual(vf) => originating_file_path(db, vf.parent?),
+        FileLongId::External(id) => originating_file_path(db, db.try_ext_as_virtual(id)?.parent?),
     }
 }
 
@@ -87,18 +117,8 @@ pub fn clear_old_diagnostics(
     notifier: Notifier,
 ) {
     let removed = project_diagnostics.clear_old(&files_to_preserve);
-    for mut file_diagnostics in removed {
-        // It might be that we are removing a file that actually had some diagnostics.
-        // For example, this might happen if a file with a syntax error is deleted.
-        // We are reusing just removed `FileDiagnostics` instead of constructing a fresh one
-        // to preserve any extra state it might contain.
-        file_diagnostics.clear();
-
-        let params = PublishDiagnosticsParams {
-            uri: file_diagnostics.url,
-            diagnostics: vec![],
-            version: None,
-        };
+    for url in removed {
+        let params = PublishDiagnosticsParams { uri: url, diagnostics: vec![], version: None };
         notifier.notify::<PublishDiagnostics>(params);
     }
 }
