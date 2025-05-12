6f170c9ddd6fb3ffc4626e1b879df3ca2857eb36
piotmag769 <piotrmagiera150@gmail.com>
2025-05-12 12:47:03 +0200

    Fix parser and vfs modules diagnostics
    Closes #142
    
    commit-id:a38e771c

--- src/lang/diagnostics/file_batches.rs
+++ src/lang/diagnostics/file_batches.rs
@@ -3,36 +3,42 @@ use std::num::NonZero;
 
 use cairo_lang_defs::db::DefsGroup;
 use cairo_lang_filesystem::db::FilesGroup;
-use cairo_lang_filesystem::ids::FileId;
+use cairo_lang_filesystem::ids::{FileId, FileLongId};
+use cairo_lang_utils::LookupIntern;
 use lsp_types::Url;
 
 use crate::lang::db::AnalysisDatabase;
 use crate::lang::lsp::LsProtoGroup;
 
-/// Finds all analyzable files in `db` that are open and need to be analysed ASAP, thus _primary_.
+/// Finds all analyzable on disk files in `db` that are open and need to be analysed ASAP,
+/// thus _primary_.
 #[tracing::instrument(skip_all)]
 pub fn find_primary_files(db: &AnalysisDatabase, open_files: &HashSet<Url>) -> HashSet<FileId> {
     open_files
         .iter()
-        // We only want to process on disk files.
-        // Relevant virtual files will be processed as a result of processing on disk files.
-        .filter(|uri| uri.scheme() != "vfs")
         .filter_map(|uri| db.file_for_url(uri))
-        // Filter out files that don't belong to any crate, e.g. removed modules.
-        .filter(|file_id| db.file_modules(*file_id).is_ok())
+        // 1. Filter out files that don't belong to any crate, e.g. removed modules.
+        // 2. We only want to process on disk files.
+        //    Relevant virtual files will be processed as a result of processing on disk files.
+        .filter(|file_id| {
+            db.file_modules(*file_id).is_ok()
+                && matches!(file_id.lookup_intern(db), FileLongId::OnDisk(_))
+        })
         .collect()
 }
 
-/// Finds all analyzable files in `db` that are **not** primary.
+/// Finds all analyzable on disk files in `db` that are **not** primary.
 #[tracing::instrument(skip_all)]
 pub fn find_secondary_files(db: &AnalysisDatabase, primary_files: &HashSet<FileId>) -> Vec<FileId> {
     let mut result = HashSet::new();
     for crate_id in db.crates() {
         for module_id in db.crate_modules(crate_id).iter() {
-            // Schedule only module main files for refreshing.
+            // Schedule only on disk module main files for refreshing.
             // All other related files will be refreshed along with it in a single job.
             if let Ok(file) = db.module_main_file(*module_id) {
-                if !primary_files.contains(&file) {
+                if matches!(file.lookup_intern(db), FileLongId::OnDisk(_))
+                    && !primary_files.contains(&file)
+                {
                     result.insert(file);
                 }
             }
