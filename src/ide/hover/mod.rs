0caafa1a5a3b746e126bf04d3844479036feb3e7
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2025-05-29 15:14:16 +0200

    Use resultants in hover (#668)

--- src/ide/hover/mod.rs
+++ src/ide/hover/mod.rs
@@ -1,9 +1,12 @@
 use lsp_types::{Hover, HoverContents, HoverParams, MarkupContent, MarkupKind};
 
+use crate::ide::markdown::RULE;
 use crate::lang::db::{AnalysisDatabase, LsSemanticGroup, LsSyntaxGroup};
-use crate::lang::lsp::{LsProtoGroup, ToCairo};
+use crate::lang::lsp::{LsProtoGroup, ToCairo, ToLsp};
 use cairo_lang_semantic::db::SemanticGroup;
-use cairo_lang_syntax::node::ast::TerminalUnderscore;
+use cairo_lang_syntax::node::ast::{TerminalIdentifier, TerminalUnderscore};
+use cairo_lang_syntax::node::{SyntaxNode, TypedSyntaxNode};
+use cairo_lang_utils::ordered_hash_set::OrderedHashSet;
 
 mod render;
 
@@ -11,24 +14,52 @@ mod render;
 pub fn hover(params: HoverParams, db: &AnalysisDatabase) -> Option<Hover> {
     let file_id = db.file_for_url(&params.text_document_position_params.text_document.uri)?;
     let position = params.text_document_position_params.position.to_cairo();
-    let node = db.find_syntax_node_at_position(file_id, position)?;
+    // Try to apply identifier correction before resultants.
+    let node = db
+        .find_identifier_at_position(file_id, position)
+        .map(|ident| ident.as_syntax_node())
+        .or_else(|| db.find_syntax_node_at_position(file_id, position))?;
+
+    let resultants = db.get_node_resultants(node)?;
+
+    let hover_content = resultants
+        .into_iter()
+        .filter_map(|node| render_hover(db, node))
+        .collect::<OrderedHashSet<_>>() // Deduplicate so we want display doubled hover if we point to same item from few resultants.
+        .into_iter()
+        .reduce(|value1, value2| format!("{value1}\n{RULE}{value2}"))?;
+
+    let hover = Hover {
+        contents: HoverContents::Markup(MarkupContent {
+            kind: MarkupKind::Markdown,
+            value: hover_content,
+        }),
+        range: node.span_without_trivia(db).position_in_file(db, file_id).map(|p| p.to_lsp()),
+    };
+
+    Some(hover)
+}
+
+fn render_hover(db: &AnalysisDatabase, node: SyntaxNode) -> Option<String> {
+    let file_id = node.stable_ptr(db).file_id(db);
 
     let importables =
         db.visible_importables_from_module(db.find_module_file_containing_node(&node)?)?;
 
-    if let Some(hover) = render::literal(db, &node, file_id, &importables) {
+    if let Some(hover) = render::literal(db, node, &importables) {
         return Some(hover);
     }
 
     if let Some(hover) = node
         .ancestor_of_type::<TerminalUnderscore>(db)
-        .and_then(|underscore| render::ty(db, underscore, file_id, &importables))
+        .and_then(|underscore| render::ty(db, underscore, &importables))
     {
         return Some(hover);
     }
 
-    if let Some(hover) = db
-        .find_identifier_at_position(file_id, position)
+    if let Some(hover) = node
+        .ancestors_with_self(db)
+        .find_map(|node| TerminalIdentifier::cast(db, node))
         .and_then(|ref id| render::definition(db, id, file_id, &importables))
     {
         return Some(hover);
@@ -37,9 +68,4 @@ pub fn hover(params: HoverParams, db: &AnalysisDatabase) -> Option<Hover> {
     None
 
     // TODO(mkaput): If client only supports plaintext, strip markdown formatting here like RA.
-}
-
-/// Convenience shortcut for building hover contents from markdown block.
-pub fn markdown_contents(md: String) -> HoverContents {
-    HoverContents::Markup(MarkupContent { kind: MarkupKind::Markdown, value: md })
 }
