caa3990b3d44136e522a706849cb49d3ed59a1b4
Piotr Magiera <56825108+piotmag769@users.noreply.github.com>
2025-06-05 18:21:47 +0200

    Add support for macro tests (#719)

--- tests/e2e/support/insta/mod.rs
+++ tests/e2e/support/insta/mod.rs
@@ -13,4 +13,41 @@ macro_rules! test_transform {
     }};
 }
 
+#[expect(unused_macros)]
+macro_rules! test_transform_inner {
+    ($transform_type:ty, $fixture:expr, $before:literal, @$after:literal, $with_macros:expr) => {{
+        let before = ::indoc::indoc!($before);
+        let description = ::std::format!(
+            "// transform: {transform}\n{before}",
+            transform = stringify!($transform),
+            before = before.trim_end(),
+        );
+
+        let after = crate::support::transform::conduct_transformation::<$transform_type>(&before, $with_macros, $fixture);
+        ::insta::with_settings!({ description => description }, {
+            ::insta::assert_snapshot!(after, @$after);
+        });
+    }};
+}
+
+#[expect(unused_macros)]
+macro_rules! test_transform_plain {
+    ($transform_type:ty, $fixture:expr, $before:literal, @$after:literal) => {{
+        test_transform_inner!($transform_type, $fixture, $before, @$after, false);
+    }};
+    ($transform_type:ty, $before:literal, @$after:literal) => {
+        test_transform_plain!($transform_type, crate::support::fixture::Fixture::new(), $before, @$after)
+    };
+}
+
+#[expect(unused_macros)]
+macro_rules! test_transform_with_macros {
+    ($transform_type:ty, $fixture:expr, $before:literal, @$after:literal) => {{
+        test_transform_inner!($transform_type, $fixture, $before, @$after, true);
+    }};
+    ($transform_type:ty, $before:literal, @$after:literal) => {
+        test_transform_with_macros!($transform_type, crate::support::fixture::Fixture::new(), $before, @$after)
+    };
+}
+
 pub(crate) use test_transform;
