327f939ba7e5483a4b7671bb5aa4670b2919d865
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2025-06-10 12:55:53 +0200

    Use Transformer in hover tests
    ✈️
    
    commit-id:a7163a86

--- tests/e2e/hover/mod.rs
+++ tests/e2e/hover/mod.rs
@@ -6,9 +6,9 @@ use lsp_types::{
 };
 use serde::Serialize;
 
-use crate::support::cairo_project_toml::CAIRO_PROJECT_TOML_2023_11;
-use crate::support::cursor::{peek_caret, peek_selection};
-use crate::support::{cursors, sandbox};
+use crate::support::MockClient;
+use crate::support::cursor::{Cursors, peek_caret, peek_selection};
+use crate::support::transform::Transformer;
 
 mod basic;
 mod consts;
@@ -22,39 +22,43 @@ mod traits;
 mod types;
 mod variables;
 
-/// Perform hover test.
-///
-/// This function spawns a sandbox language server with the given code in the `src/lib.cairo` file.
-/// The Cairo source code is expected to contain caret markers.
-/// The function then requests hover information at each caret position and compares the result with
-/// the expected hover information from the snapshot file.
-fn test_hover(cairo_code: &str) -> Report {
-    let (cairo, cursors) = cursors(cairo_code);
-
-    let mut ls = sandbox! {
-        files {
-            "cairo_project.toml" => CAIRO_PROJECT_TOML_2023_11,
-            "src/lib.cairo" => cairo.clone(),
+impl Transformer for Hover {
+    fn capabilities(base: ClientCapabilities) -> ClientCapabilities {
+        ClientCapabilities {
+            text_document: base.text_document.or_else(Default::default).map(|it| {
+                TextDocumentClientCapabilities {
+                    hover: Some(HoverClientCapabilities {
+                        dynamic_registration: Some(false),
+                        content_format: Some(vec![MarkupKind::Markdown, MarkupKind::PlainText]),
+                    }),
+                    ..it
+                }
+            }),
+            ..base
         }
-        client_capabilities = caps;
-    };
+    }
 
-    ls.open_all_cairo_files_and_wait_for_project_update();
+    fn transform(mut ls: MockClient, cursors: Cursors) -> String {
+        let cairo = ls.fixture.read_file("src/lib.cairo");
+        let position = cursors.assert_single_caret();
 
-    let position = cursors.assert_single_caret();
+        let hover = ls.send_request::<lsp_request!("textDocument/hover")>(HoverParams {
+            text_document_position_params: TextDocumentPositionParams {
+                text_document: ls.doc_id("src/lib.cairo"),
+                position,
+            },
+            work_done_progress_params: Default::default(),
+        });
 
-    let hover = ls.send_request::<lsp_request!("textDocument/hover")>(HoverParams {
-        text_document_position_params: TextDocumentPositionParams {
-            text_document: ls.doc_id("src/lib.cairo"),
-            position,
-        },
-        work_done_progress_params: Default::default(),
-    });
-
-    Report {
-        source_context: peek_caret(&cairo, position),
-        highlight: hover.as_ref().and_then(|h| h.range).map(|range| peek_selection(&cairo, &range)),
-        popover: hover.map(render),
+        Report {
+            source_context: peek_caret(&cairo, position),
+            highlight: hover
+                .as_ref()
+                .and_then(|h| h.range)
+                .map(|range| peek_selection(&cairo, &range)),
+            popover: hover.map(render),
+        }
+        .to_string()
     }
 }
 
@@ -83,20 +87,5 @@ fn render(h: Hover) -> String {
         contents => {
             panic!("LS returned deprecated MarkedString-based hover contents: {:#?}", contents);
         }
-    }
-}
-
-fn caps(base: ClientCapabilities) -> ClientCapabilities {
-    ClientCapabilities {
-        text_document: base.text_document.or_else(Default::default).map(|it| {
-            TextDocumentClientCapabilities {
-                hover: Some(HoverClientCapabilities {
-                    dynamic_registration: Some(false),
-                    content_format: Some(vec![MarkupKind::Markdown, MarkupKind::PlainText]),
-                }),
-                ..it
-            }
-        }),
-        ..base
     }
 }
