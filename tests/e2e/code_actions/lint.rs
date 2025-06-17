e16643bb076f7be1100b6bebf5895368634d9e88
piotmag769 <piotrmagiera150@gmail.com>
2025-06-17 16:07:22 +0200

    Revert "Add description to lint fix diagnostics (#755)"
    This reverts commit 2347efbe8856f08613db462d3b33a39cb6fe677e.

--- tests/e2e/code_actions/lint.rs
+++ tests/e2e/code_actions/lint.rs
@@ -10,7 +10,7 @@ fn check_for_lint() {
         }
     }
     ", @r#"
-    Title: Remove unnecessary parentheses from break
+    Title: Fix lint
     Add new text: "        break;
     "
     At: Range { start: Position { line: 2, character: 0 }, end: Position { line: 3, character: 0 } }
