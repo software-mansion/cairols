678685bd86ad8183789cc422b2c71c0e4514ef50
Jan Smółka <jp.smolka@gmail.com>
2025-07-02 13:44:37 +0200

    Fix highlighting in code controlled by proc macros

--- src/ide/navigation/highlight.rs
+++ src/ide/navigation/highlight.rs
@@ -1,7 +1,10 @@
+use cairo_lang_filesystem::ids::FileId;
+use cairo_lang_syntax::node::TypedSyntaxNode;
+use cairo_lang_syntax::node::ast::TerminalIdentifier;
 use itertools::Itertools;
 use lsp_types::{DocumentHighlight, DocumentHighlightParams};
 
-use crate::lang::db::{AnalysisDatabase, LsSyntaxGroup};
+use crate::lang::db::{AnalysisDatabase, LsSemanticGroup, LsSyntaxGroup};
 use crate::lang::defs::SymbolSearch;
 use crate::lang::lsp::{LsProtoGroup, ToCairo, ToLsp};
 use crate::lang::usages::search_scope::SearchScope;
@@ -13,9 +16,29 @@ pub fn highlight(
     let file = db.file_for_url(&params.text_document_position_params.text_document.uri)?;
     let position = params.text_document_position_params.position.to_cairo();
 
-    let identifier = db.find_identifier_at_position(file, position)?;
+    let identifier = db.find_identifier_at_position(file, position)?.as_syntax_node();
 
-    let symbol_search = SymbolSearch::find_definition(db, &identifier)?;
+    let highlights = db
+        .get_node_resultants(identifier)
+        .unwrap_or_else(|| vec![identifier])
+        .into_iter()
+        .filter_map(|node| {
+            let id = node.cast::<TerminalIdentifier>(db)?;
+            identifier_highlights(db, &id, file)
+        })
+        .flatten()
+        .collect();
+
+    Some(highlights)
+}
+
+/// Finds positions in the file to highlight as references to `identifier`.
+fn identifier_highlights(
+    db: &AnalysisDatabase,
+    identifier: &TerminalIdentifier,
+    file: FileId,
+) -> Option<Vec<DocumentHighlight>> {
+    let symbol_search = SymbolSearch::find_definition(db, identifier)?;
 
     let highlights = symbol_search
         .usages(db)
