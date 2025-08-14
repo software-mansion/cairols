82018f14aa7c94813e1582089c10497529d04e87
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2025-08-14 13:45:32 +0200

    2.12.1 Completions of items from `Deref` impls (#897)

--- src/ide/completion/dot_completions.rs
+++ src/ide/completion/dot_completions.rs
@@ -6,8 +6,9 @@ use cairo_lang_semantic::db::SemanticGroup;
 use cairo_lang_semantic::items::function_with_body::SemanticExprLookup;
 use cairo_lang_semantic::lookup_item::LookupItemEx;
 use cairo_lang_semantic::types::peel_snapshots;
-use cairo_lang_semantic::{ConcreteTypeId, TypeLongId};
+use cairo_lang_semantic::{ConcreteTypeId, TypeId, TypeLongId};
 use cairo_lang_syntax::node::{TypedStablePtr, TypedSyntaxNode, ast};
+use itertools::{Itertools, chain};
 use lsp_types::{CompletionItem, CompletionItemKind, InsertTextFormat};
 use tracing::debug;
 
@@ -16,7 +17,6 @@ use crate::lang::analysis_context::AnalysisContext;
 use crate::lang::db::AnalysisDatabase;
 use crate::lang::importer::new_import_edit;
 use crate::lang::methods::find_methods_for_type;
-use itertools::Itertools;
 
 pub fn dot_completions(
     db: &AnalysisDatabase,
@@ -26,7 +26,7 @@ pub fn dot_completions(
     // Get a resolver in the current context.
     let function_with_body = ctx.lookup_item_id?.function_with_body()?;
     let module_file_id = function_with_body.module_file_id(db);
-    let resolver = ctx.resolver(db);
+    let mut resolver = ctx.resolver(db);
     let importables = db.visible_importables_from_module(ctx.module_file_id)?;
 
     // Extract lhs node.
@@ -42,32 +42,45 @@ pub fn dot_completions(
         return None;
     }
 
-    // Find relevant methods for type.
-    let relevant_methods = find_methods_for_type(db, resolver, ty, stable_ptr);
-
     let mut completions = Vec::new();
-    for trait_function in relevant_methods {
-        let Some(completion) = completion_for_method(db, ctx, module_file_id, trait_function)
-        else {
-            continue;
-        };
-        completions.push(completion);
-    }
 
-    // Find members of the type.
-    let (_, long_ty) = peel_snapshots(db, ty);
-    if let TypeLongId::Concrete(ConcreteTypeId::Struct(concrete_struct_id)) = long_ty {
-        db.concrete_struct_members(concrete_struct_id).ok()?.iter().for_each(|(name, member)| {
-            let completion = CompletionItem {
-                label: name.to_string(),
-                detail: Some(format_type(db, member.ty, &importables)),
-                kind: Some(CompletionItemKind::FIELD),
-                ..CompletionItem::default()
+    let types = deref_targets(db, ty)?;
+
+    for ty in types {
+        // Find relevant methods for type.
+        let relevant_methods = find_methods_for_type(db, &mut resolver, ty, stable_ptr);
+
+        for trait_function in relevant_methods {
+            let Some(completion) = completion_for_method(db, ctx, module_file_id, trait_function)
+            else {
+                continue;
             };
             completions.push(completion);
-        });
+        }
+
+        // Find members of the type.
+        let (_, long_ty) = peel_snapshots(db, ty);
+        if let TypeLongId::Concrete(ConcreteTypeId::Struct(concrete_struct_id)) = long_ty {
+            db.concrete_struct_members(concrete_struct_id).ok()?.iter().for_each(
+                |(name, member)| {
+                    let completion = CompletionItem {
+                        label: name.to_string(),
+                        detail: Some(format_type(db, member.ty, &importables)),
+                        kind: Some(CompletionItemKind::FIELD),
+                        ..CompletionItem::default()
+                    };
+                    completions.push(completion);
+                },
+            );
+        }
     }
     Some(completions)
+}
+
+fn deref_targets(db: &AnalysisDatabase, ty: TypeId) -> Option<Vec<TypeId>> {
+    let deref_chain = db.deref_chain(ty, true).ok()?;
+
+    Some(chain!(Some(ty), deref_chain.derefs.iter().map(|info| info.target_ty)).collect())
 }
 
 /// Returns a completion item for a method.
