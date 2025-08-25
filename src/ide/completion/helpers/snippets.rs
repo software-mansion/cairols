88708a3eae9885cbb2ab55c943d457ea619a0ba8
piotmag769 <piotrmagiera150@gmail.com>
2025-08-25 14:59:22 +0200

    Use snippets in dot method completions
    Related to #925
    
    commit-id:7a731d70

---
+++ src/ide/completion/helpers/snippets.rs
@@ -0,0 +1,20 @@
+use cairo_lang_semantic::Signature;
+use itertools::Itertools;
+
+/// Creates an LSP snippet for the function with the given name the signature.
+///
+/// Example: for a function with a signature
+/// ```cairo
+/// fn xyz(a: u8, b: ByteArray) -> felt252 {}
+/// ```
+/// returns a string "xyz({$1:a}, {$2:b})".
+pub fn snippet_for_function_call(function_name: &str, signature: Signature) -> String {
+    let params_snippet = signature
+        .params
+        .into_iter()
+        .filter(|param| param.name != "self")
+        .enumerate()
+        .map(|(index, param)| format!("${{{}:{}}}", index + 1, param.name))
+        .join(", ");
+    format!("{function_name}({params_snippet})")
+}
