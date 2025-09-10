d95a48048559d4508c5e40c9ea94d56c54f6729e
Piotr Magiera <56825108+piotmag769@users.noreply.github.com>
2025-09-10 23:29:58 +0200

    Find ref tests for declarative macros (#972)

--- tests/e2e/find_references/macros.rs
+++ tests/e2e/find_references/macros.rs
@@ -3,7 +3,7 @@ use lsp_types::request::References;
 use crate::support::insta::{test_transform_plain, test_transform_with_macros};
 
 #[test]
-fn inline_macro() {
+fn plugin_inline_macro() {
     test_transform_plain!(References, r#"
     fn main() {
         print!("Hello world!");
@@ -25,7 +25,7 @@ fn inline_macro() {
 }
 
 #[test]
-fn inline_macro_with_macros() {
+fn plugin_inline_macro_with_macros() {
     test_transform_with_macros!(References, r#"
     #[complex_attribute_macro_v2]
     fn main() {
@@ -46,6 +46,129 @@ fn inline_macro_with_macros() {
 
     fn bar() {
         let forty_two = <sel>array</sel>![42];
+    }
+    "#)
+}
+
+#[test]
+fn declarative_inline_macro_on_usage() {
+    test_transform_plain!(References, r#"
+    fn main() {
+        print!("Hello world!");
+        let x = inc!(0);
+        let y = other_inc!(0);
+    }
+    fn bar() {
+        let forty_two = in<caret>c!(5);
+    }
+
+    pub macro inc {
+        ($x:expr) => { $x + 1 };
+    }
+
+    pub macro other_inc {
+        ($x:expr) => { $x + 1 };
+    }
+    "#, @r#"
+    fn main() {
+        print!("Hello world!");
+        let x = <sel>inc</sel>!(0);
+        let y = other_inc!(0);
+    }
+    fn bar() {
+        let forty_two = <sel>inc</sel>!(5);
+    }
+
+    pub macro <sel=declaration>inc</sel> {
+        ($x:expr) => { $x + 1 };
+    }
+
+    pub macro other_inc {
+        ($x:expr) => { $x + 1 };
+    }
+    "#)
+}
+
+#[test]
+fn declarative_inline_macro_on_definition() {
+    test_transform_plain!(References, r#"
+    fn main() {
+        print!("Hello world!");
+        let x = inc!(0);
+        let y = other_inc!(0);
+    }
+    fn bar() {
+        let forty_two = inc!(5);
+    }
+
+    pub macro i<caret>nc {
+        ($x:expr) => { $x + 1 };
+    }
+
+    pub macro other_inc {
+        ($x:expr) => { $x + 1 };
+    }
+    "#, @r#"
+    fn main() {
+        print!("Hello world!");
+        let x = <sel>inc</sel>!(0);
+        let y = other_inc!(0);
+    }
+    fn bar() {
+        let forty_two = <sel>inc</sel>!(5);
+    }
+
+    pub macro <sel=declaration>inc</sel> {
+        ($x:expr) => { $x + 1 };
+    }
+
+    pub macro other_inc {
+        ($x:expr) => { $x + 1 };
+    }
+    "#)
+}
+
+#[test]
+fn declarative_inline_macro_on_usage_with_macros() {
+    test_transform_with_macros!(References, r#"
+    fn main() {
+        print!("Hello world!");
+        let x = inc!(0);
+        let y = other_inc!(0);
+    }
+
+    #[complex_attribute_macro_v2]
+    fn bar() {
+        let forty_two = in<caret>c!(5);
+    }
+
+    #[complex_attribute_macro_v2]
+    pub macro inc {
+        ($x:expr) => { $x + 1 };
+    }
+
+    pub macro other_inc {
+        ($x:expr) => { $x + 1 };
+    }
+    "#, @r#"
+    fn main() {
+        print!("Hello world!");
+        let x = <sel>inc</sel>!(0);
+        let y = other_inc!(0);
+    }
+
+    #[complex_attribute_macro_v2]
+    fn bar() {
+        let forty_two = <sel>inc</sel>!(5);
+    }
+
+    #[complex_attribute_macro_v2]
+    pub macro <sel=declaration>inc</sel> {
+        ($x:expr) => { $x + 1 };
+    }
+
+    pub macro other_inc {
+        ($x:expr) => { $x + 1 };
     }
     "#)
 }
