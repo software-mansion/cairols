708542c151d23101e3c7bbf2fa4c6d615c65431e
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2025-09-17 16:43:20 +0200

    Fix `integration_test_with_fork_and_fuzzer`
    commit-id:9bc75290

--- tests/e2e/macros/procedural/snforge/mod.rs
+++ tests/e2e/macros/procedural/snforge/mod.rs
@@ -44,7 +44,6 @@ fn unit_test_with_assert_macro() {
 }
 
 #[test]
-#[ignore] // probably wrong corelib version
 fn integration_test_with_fork_and_fuzzer() {
     test_macro_expansion_and_diagnostics!(
         project = ProjectWithSnforgeIntegrationTest,
