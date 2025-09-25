9b1162a74eeb2544f28fc4d86300a46833081bb2
Tomasz Rejowski <34059895+Arcticae@users.noreply.github.com>
2025-09-25 15:44:47 +0200

    Fix executable lens placement (#1007)

--- tests/e2e/code_lens/executable.rs
+++ tests/e2e/code_lens/executable.rs
@@ -28,6 +28,37 @@ fn test_one_executable_whole_package() {
 }
 
 #[test]
+fn test_one_executable_with_trivia() {
+    insta::assert_snapshot!(test_code_lens_scarb_execute((
+        r#"
+        fn mock() {
+
+        }
+
+
+
+        #[executable]
+        fn main<caret>(){
+            // Do stuff
+        }
+        "#,
+        indoc!(
+            r#"
+            [package]
+            name = "indor"
+            version = "0.1.0"
+            edition = "2024_07"
+
+            [dependencies]
+            cairo_execute = "2.9.0"
+
+            [executable]
+            "#
+        )
+    )));
+}
+
+#[test]
 fn test_two_executables_whole_package() {
     insta::assert_snapshot!(test_code_lens_scarb_execute((
         r#"
