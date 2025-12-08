07c2cbbfbe770944d5ed1ad072ec0ec7450d4ad8
Wojciech Szymczyk <wojciech.szymczyk@swmansion.com>
2025-12-08 09:38:25 +0100

    Fix tests

--- tests/e2e/scarb/unmanaged_core.rs
+++ tests/e2e/scarb/unmanaged_core.rs
@@ -29,7 +29,7 @@ fn test_unmanaged_core_on_invalid_scarb_toml() {
       "settings": {
         "name": null,
         "edition": "2025_12",
-        "version": "2.13.1",
+        "version": "2.14.0",
         "cfg_set": null,
         "dependencies": {},
         "experimental_features": {
