ca8dd24129fad9f263ec00f71e39ac03715bc4d4
Wojciech Szymczyk <wojciech.szymczyk@swmansion.com>
2025-12-19 12:53:05 +0100

    construct empty span before get_originating_location

--- src/ide/completion/helpers/span.rs
+++ src/ide/completion/helpers/span.rs
@@ -1,5 +1,6 @@
 use cairo_lang_filesystem::db::get_originating_location;
 use cairo_lang_filesystem::ids::SpanInFile;
+use cairo_lang_filesystem::span::TextSpan;
 use cairo_lang_syntax::node::SyntaxNode;
 use cairo_lang_syntax::node::kind::SyntaxKind;
 use lsp_types::Range;
@@ -26,13 +27,16 @@ pub fn get_empty_arglist_range<'db>(
     resultant: SyntaxNode<'db>,
 ) -> Option<Range> {
     let arg_list = resultant.ancestor_of_kind(db, SyntaxKind::ArgList)?;
+    let arg_list_span = arg_list.span(db);
+    let empty_span = TextSpan::new(arg_list_span.end, arg_list_span.end);
+
     let arg_list_span =
-        SpanInFile { file_id: arg_list.stable_ptr(db).file_id(db), span: arg_list.span(db) };
+        SpanInFile { file_id: arg_list.stable_ptr(db).file_id(db), span: empty_span };
 
     let span_in_file = get_originating_location(db, arg_list_span, None);
 
     Some(Range {
-        start: span_in_file.span.end.position_in_file(db, span_in_file.file_id)?.to_lsp(),
+        start: span_in_file.span.start.position_in_file(db, span_in_file.file_id)?.to_lsp(),
         end: span_in_file.span.end.position_in_file(db, span_in_file.file_id)?.to_lsp(),
     })
 }
