ef7004b1638d2f167ecf70c4661f98adc8a0ead0
Jan Smółka <jp.smolka@gmail.com>
2026-04-08 15:11:23 +0200

    Fix proto group test

--- src/lang/lsp/ls_proto_group_test.rs
+++ src/lang/lsp/ls_proto_group_test.rs
@@ -8,7 +8,7 @@ use crate::lang::{db::AnalysisDatabase, lsp::LsProtoGroup};
 fn file_url() {
     let db = &AnalysisDatabase::new();
 
-    let check = |expected_url: &str, expected_file_long: FileLongId| {
+    let check_on_disk = |expected_url: &str, expected_file_long: FileLongId| {
         let expected_url = Url::parse(expected_url).unwrap();
         let expected_file = expected_file_long.intern(db);
 
@@ -26,13 +26,26 @@ fn file_url() {
         );
     };
 
-    check("file:///foo/bar", FileLongId::OnDisk("/foo/bar".into()));
-    check("file:///", FileLongId::OnDisk("/".into()));
+    let check_virtual = |expected_path: &str, expected_file_long: FileLongId| {
+        let expected_file = expected_file_long.intern(db);
+        let actual_url = db.url_for_file(expected_file).unwrap();
+        let expected_host = expected_file.as_intern_id().index().to_string();
 
-    // NOTE: We expect that Salsa is assigning sequential numeric ids to files,
-    //   hence numbers 9218 and 9219 appear further down.
-    check(
-        "vfs://9218/foo.cairo",
+        assert_eq!(db.file_for_url(&actual_url), Some(expected_file), "just use {actual_url}");
+        assert_eq!(actual_url.scheme(), "vfs");
+        assert_eq!(
+            actual_url.host_str(),
+            Some(expected_host.as_str()),
+            "unexpected host in {actual_url}"
+        );
+        assert_eq!(actual_url.path(), expected_path, "unexpected path in {actual_url}");
+    };
+
+    check_on_disk("file:///foo/bar", FileLongId::OnDisk("/foo/bar".into()));
+    check_on_disk("file:///", FileLongId::OnDisk("/".into()));
+
+    check_virtual(
+        "/foo.cairo",
         FileLongId::Virtual(VirtualFile {
             parent: None,
             name: SmolStrId::from(db, "foo"),
@@ -42,8 +55,8 @@ fn file_url() {
             original_item_removed: false,
         }),
     );
-    check(
-        "vfs://9219/foo%2Fbar.cairo",
+    check_virtual(
+        "/foo%2Fbar.cairo",
         FileLongId::Virtual(VirtualFile {
             parent: None,
             name: SmolStrId::from(db, "foo/bar"),
