d27def852e198a8924a593ad63f26788fa109013
Maciej Trątnowiecki <maciej.tratnowiecki@swmansion.com>
2026-04-10 21:12:27 +0200

    Support type info
    Add tests
    Assert no type info outside function body
    
    Assert built-in inline macros do not work
    
    Assert proc inline and attribute macros work
    
    Fix impl for proc macros
    
    Refactor
    
    Fix type info range inside macro generated files
    
    Show full type signature in type info hovers
    
    Review fixes

--- src/ide/hover/render/mod.rs
+++ src/ide/hover/render/mod.rs
@@ -1,9 +1,11 @@
 pub use self::definition::*;
+pub use self::expression::type_info;
 pub use self::keyword::*;
 pub use self::literal::*;
 pub use self::ty::*;
 
 mod definition;
+mod expression;
 mod keyword;
 mod literal;
 mod ty;
